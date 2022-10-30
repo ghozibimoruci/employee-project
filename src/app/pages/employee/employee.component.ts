b62b27e81f1da822aafdd14a18e1b32daa88dbbe
ghozibimoruci <56252960+v-ghozi.bimo@anteraja.id>
2022-10-30 07:25:25 +0700

    tic tac toe

--- src/app/pages/employee/employee.component.ts
+++ src/app/pages/employee/employee.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from "@angular/core";
 import * as moment from "moment";
 import { sortArray } from "../../shared/shared_file";
 import { firstNameF, firstNameM, groupName, lastNameF, lastNameM } from '../../shared/base-file';
+import { FormBuilder, FormControl, Validators } from "@angular/forms";
 
 @Component({
     selector: 'employee-component',
@@ -30,8 +31,16 @@ export class EmployeeComponent implements OnInit {employeeList=[];
 
     editedEmployee = null;
     existingEmail = [];
-    showList=true;
+    showList = true;
+    constructor(
+      private formBuilder : FormBuilder
+    ){
+    }
     ngOnInit(): void {
+      this.formBuilder.group({
+        userName: [null, [Validators.required, Validators.maxLength(50)]],
+        passWord: [null, [Validators.required]]
+      })
         this.generateEmployeeName(this.totalList);
         this.filterEmpoyeeList();
     }
@@ -65,36 +74,42 @@ export class EmployeeComponent implements OnInit {employeeList=[];
     }
   
     generateEmployeeName(lengthList: number){
+      let existingEmployeeList = this.getLocalList();
       this.baseEmployeeList = [];
-      while(this.baseEmployeeList.length<lengthList){
-        let eAge = this.getRandomAge();
-        let isMale = this.getRandomBoolean();
-        let dateBorn = this.getRandomNumber(1, 31),
-        monthBorn = this.getRandomNumber(1, 12),
-        yearBorn = moment().year() - eAge;
-        let birthDate = moment(monthBorn+'-'+dateBorn+'-'+yearBorn).format('MMMM DD, YYYY');
-        let birthDateSort = moment(monthBorn+'-'+dateBorn+'-'+yearBorn).format('YYYY-MMMM-DD');
-        let firstName = this['firstName'+(isMale?'M':'F')][this.getRandomNumber(1, 15) - 1];
-        let lastName = this['lastName'+(isMale?'M':'F')][this.getRandomNumber(1, 15) - 1];
-        let username = firstName.toLowerCase()+'.'+lastName.toLowerCase();
-        username = this.generateEmployeeUsername(username)+'@gmail.com';
-        let basicSalary = this.getRandomNumber(8, 26)*1000000;
-        let group = groupName[this.getRandomNumber(1, 10) - 1];
-        let status = this.getRandomNumber(1, 20) > 18?'inactive':'active';
-        this.baseEmployeeList.push({
-          id: this.baseEmployeeList.length,
-          username,
-          firstName,
-          lastName,
-          fullname: firstName+' '+lastName,
-          email: username,
-          birthDate,
-          birthDateSort, 
-          basicSalary,
-          group,
-          status,
-          description: moment().format('DD-MM-YYYY')
-         })
+      if(existingEmployeeList){
+        this.baseEmployeeList = existingEmployeeList;
+      }else{
+        while(this.baseEmployeeList.length<lengthList){
+          let eAge = this.getRandomAge();
+          let isMale = this.getRandomBoolean();
+          let dateBorn = this.getRandomNumber(1, 31),
+          monthBorn = this.getRandomNumber(1, 12),
+          yearBorn = moment().year() - eAge;
+          let birthDate = moment(monthBorn+'-'+dateBorn+'-'+yearBorn).format('MMMM DD, YYYY');
+          let birthDateSort = moment(monthBorn+'-'+dateBorn+'-'+yearBorn).format('YYYY-MMMM-DD');
+          let firstName = this['firstName'+(isMale?'M':'F')][this.getRandomNumber(1, 15) - 1];
+          let lastName = this['lastName'+(isMale?'M':'F')][this.getRandomNumber(1, 15) - 1];
+          let username = firstName.toLowerCase()+'.'+lastName.toLowerCase();
+          username = this.generateEmployeeUsername(username)+'@gmail.com';
+          let basicSalary = this.getRandomNumber(8, 26)*1000000;
+          let group = groupName[this.getRandomNumber(1, 10) - 1];
+          let status = this.getRandomNumber(1, 20) > 18?'inactive':'active';
+          this.baseEmployeeList.push({
+            id: this.baseEmployeeList.length,
+            username,
+            firstName,
+            lastName,
+            fullname: firstName+' '+lastName,
+            email: username,
+            birthDate,
+            birthDateSort,
+            basicSalary,
+            group,
+            status,
+            description: moment().format('DD-MM-YYYY')
+           })
+        }
+        this.setLocalList();
       }
     }
 
@@ -161,7 +176,16 @@ export class EmployeeComponent implements OnInit {employeeList=[];
           this.baseEmployeeList[editedIndex] = event;
         }
       }
+      this.setLocalList();
       this.showList=true;
       this.filterEmpoyeeList();
+    }
+
+    getLocalList(){
+      return JSON.parse(localStorage.getItem('employee-list')||'null');
+    }
+
+    setLocalList(){
+      localStorage.setItem('employee-list', JSON.stringify(this.baseEmployeeList));
     }
 }
